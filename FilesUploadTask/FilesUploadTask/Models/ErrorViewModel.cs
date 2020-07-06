8971774b5e46fc94f33827dd0eab7c05af2187ad
Alexander Zhadetskij <coolweak2@gmail.com>
2020-07-06 15:41:22 +0300

    Initial commit

---
+++ FilesUploadTask/FilesUploadTask/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FilesUploadTask.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
