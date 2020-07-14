3c4204ec352c568bb2cb8cd76bc6a37a4775628e
Alexander Zhadetskij <coolweak2@gmail.com>
2020-07-14 14:45:07 +0300

    image retrieve service & controller edited

--- app/app.services/Constants/Constants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace app.services.Constants
-{
-    public static class Constants
-    {
-        public static readonly string UploadsFolder = "uploads";
-        public static readonly int InvalidCategory = 0;
-    }
-}
